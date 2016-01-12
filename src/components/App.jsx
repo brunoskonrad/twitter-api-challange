0d98186baff5c556c4b165266be2ac2012e7ab8c
Bruno Konrad <bruno.skon@gmail.com>
2016-01-12 15:22:59 -0200

    Add footer; add loading circle; improve layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from 'components/SearchBar.jsx';
 import TweetsList from 'components/TweetsList.jsx';
+import Footer from 'components/Footer.jsx';
 
 const App = React.createClass({
   getInitialState() {
@@ -10,15 +11,24 @@ const App = React.createClass({
   },
 
   searchTweets(twitterName) {
+    this.setState({
+      tweets: [],
+      isLoading: true
+    });
     fetch(`/tweets/${twitterName}`)
       .then((response) => {
         return response.json();
       })
       .then((tweets) => {
         this.setState({
-          tweets
+          tweets,
+          isLoading: false
         });
       });
+  },
+
+  renderLoadingTweets() {
+    return (<div className="app__loading"></div>);
   },
 
   render() {
@@ -27,6 +37,8 @@ const App = React.createClass({
         <SearchBar onSearch={this.searchTweets} />
         {this.state.tweets.length > 0 &&
           <TweetsList tweets={this.state.tweets} />}
+        {this.state.isLoading && this.renderLoadingTweets()}
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = React.createClass({
+  render() {
+    return (
+      <footer className="footer">
+        <span>Powered by</span>
+        <div className="footer__twitter"></div>
+      </footer>
+    );
+  }
+});
+
+export default Footer;
